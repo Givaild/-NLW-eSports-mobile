055d4e1e236cac4e372f5f78219646520efc88e9
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-09-26 10:48:02 -0300

    fix:Create new components and connect with serve

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,33 @@
-import { View, Image,FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect,useState } from 'react';
+import { Image,FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import { Background } from '../../components/Background';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Head } from '../../components/Head';
-
-import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
+
+  const[games,setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame({id, title, bannerURL}:GameCardProps) {
+    navigation.navigate('game',{id, title, bannerURL});
+  }
+
+  useEffect(()=> {
+    fetch('http://192.168.1.105:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  },[]);
   return (
-    <View style={styles.container}>
+    <Background>
+    <SafeAreaView style={styles.container}>
       <Image
       source={logoImg}
       style={styles.logo}
@@ -21,21 +38,20 @@ export function Home() {
       subtitle='Selecione o game que deseja jogar...'
       />
         <FlatList 
-          data={GAMES}
+          data={games}
           keyExtractor={item =>item.id}
           renderItem={({item}) => (
-            <GameCard 
-      
+            <GameCard       
               data={item}
+              onPress={() => handleOpenGame(item)}
             />
 
           )}
           showsHorizontalScrollIndicator ={false}
           horizontal
           contentContainerStyle={styles.contentList}
-        />
-
-        
-    </View>
+        />        
+    </SafeAreaView>
+    </Background>
   );
 }
